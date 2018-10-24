5709dc2703ee6fa9c6957c871d86cd0081cd032a
jhermon <suddyjerm@gmail.com>
2018-10-23 23:08:06 -0500

    exercise2

--- maze.js
+++ maze.js
@@ -6,6 +6,21 @@ window.onload = function (){
 		boundary1.setAttribute('class', 'boundary youlose');
 	});
 
+	let boundary = document.querySelectorAll('.boundary');
+	let count = 0;
+
+	for (let x = 0; x < boundary.length; x++){
+		boundary[x].addEventListener('mouseover' ,() =>{
+			
+			for(let y = 0; y < boundary.length-1; y++){
+				boundary[y].setAttribute('class', 'boundary youlose');
+
+			}
+			count++;
+			document.getElementById('status').innerHTML = "You Lose";
+		});
+	}
+
 
 
 }
