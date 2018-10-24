4e053345273c32eecc1c414bb194f2a1bd89cd9d
jhermon <suddyjerm@gmail.com>
2018-10-23 23:46:08 -0500

    exercise5

--- maze.js
+++ maze.js
@@ -1,11 +1,12 @@
 window.onload = function (){
-
+	//execrise1
 	let boundary1 = document.getElementById('boundary1');
 
 	boundary1.addEventListener('mouseover',() => {
 		boundary1.setAttribute('class', 'boundary youlose');
 	});
 
+	//execrise2
 	let boundary = document.querySelectorAll('.boundary');
 	let count = 0;
 
@@ -17,18 +18,21 @@ window.onload = function (){
 
 			}
 			count++;
-			document.getElementById('status').innerHTML = "You Lose";
+			//prompts the user that they lost(exerxise 5)
+			document.getElementById('status').textContent = "You Lose";
 		});
 	}
 
+	//execrise3
 	let end = document.querySelector('#end');
 
 	end.addEventListener('mouseover' ,() =>{
 		if (count == 0){
-			document.querySelector('#status').innerHTML = "You Win";
+			document.querySelector('#status').textContent = "You Win";
 		}
 	});
-
+	
+	//execrise4
 	let start = document.querySelector("#start");
 
 	start.addEventListener('click' ,() =>{
