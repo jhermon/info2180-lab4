3b7395d79de16f2a48463b4b232e40f4d2961282
jhermon <suddyjerm@gmail.com>
2018-10-23 22:38:40 -0500

    lab4

---
+++ maze.js
@@ -0,0 +1,12 @@
+window.onload = function (){
+
+	let boundary1 = document.getElementById('boundary1');
+
+	boundary1.addEventListener('mouseover',() => {
+		boundary1.setAttribute('class', 'boundary youlose');
+	});
+
+
+
+}
+
